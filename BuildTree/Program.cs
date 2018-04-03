1862f7e49c35df7c27406c619be2047f92122547
Every <Every_126@outlook.com>
2018-04-03 18:51:37 +0800

    添加泛型方法来构造树

---
+++ BuildTree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Datastructure;
+
+namespace BuildTree
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Tree<char> tree = null;
+            InsertIntoTree<char>(ref tree, 'm', 'n', 's', 'd', 'a', 'g', 'g');
+            Console.WriteLine($"sorted data is {tree.WalkTree()}");
+            Console.ReadKey();
+        }
+
+        static void InsertIntoTree<TItem>(ref Tree<TItem> tree,params TItem[]data) where TItem:IComparable<TItem>
+        {
+            foreach (var datum in data)
+            {
+                if (tree==null)
+                {
+                    tree = new Tree<TItem>(datum);  
+                }
+                else
+                {
+                    tree.Insert(datum);
+                }
+            }
+        }
+    }
+}
