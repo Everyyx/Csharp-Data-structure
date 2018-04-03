b9fbd3e86af2ff4c0bbfca59751f0ff4dbfa3f94
Every <Every_126@outlook.com>
2018-04-03 18:29:44 +0800

    add tree

---
+++ BinaryTreeTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Datastructure;
+
+namespace BinaryTreeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree1 = new Tree<int>(10);
+            tree1.Insert(5);
+            tree1.Insert(3);
+            tree1.Insert(1);
+            tree1.Insert(-1);
+            tree1.Insert(-2);
+
+            tree1.Insert(-3);
+            tree1.Insert(-4);
+            tree1.Insert(-5);
+            tree1.Insert(-6);
+            tree1.Insert(-11);
+
+
+            string sortedData = tree1.WalkTree();
+            Console.WriteLine($"{sortedData}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructure
+{
+    public class Tree<TItem>where TItem:IComparable<TItem>//指定类型参数必须是实现了该泛型接口的类型
+    {
+        public Tree(TItem nodeValue)//构造器的名称不能包含类型参数
+        {
+            this.NodeData = nodeValue;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        public void Insert(TItem newItem)//递归
+        {
+            TItem currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(newItem)>0)
+            {
+                //插入左子树
+                if (this.LeftTree==null)
+                {
+                    this.LeftTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.LeftTree.Insert(newItem);
+                }
+            }
+            else
+            {
+                //插入右子树
+                if (this.RightTree==null)
+                {
+                    this.RightTree = new Tree<TItem>(newItem);              
+                }
+                else
+                {
+                    this.RightTree.Insert(newItem);
+                }
+            }
+        }
+        public string WalkTree()
+        {
+            string result = "";
+            if (this.LeftTree!=null)
+            {
+                result = this.LeftTree.WalkTree();
+            }
+            result += $"{this.NodeData.ToString()}";
+            if (this.RightTree!=null)
+            {
+                result += this.RightTree.WalkTree();
+            }
+            return result;
+        }
+        public TItem NodeData { get; set; }
+        public Tree<TItem> LeftTree { get; set; }
+        public Tree<TItem> RightTree { get; set; }
+    }
+    
+}
